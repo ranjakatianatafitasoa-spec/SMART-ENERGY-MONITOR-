1f3892063e264cb19a1a3be1fb7277024e5aaca4
ranjakatianatafitasoa-spec <ranjakatianatafitasoa@gmail.com>
2026-08-14 07:36:33 +0000

    Update project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smartenergymonitor.app',
+  appName: 'SMART ENERGY MONITOR ',
+  webDir: 'dist'
+};
+
+export default config;
